0d4cf5eced544becca3aab5b80250906da3cde62
jonatan huarte <80835787+jonaxee@users.noreply.github.com>
2023-04-19 20:41:43 -0300

    Corregida la funcion de exportar pdf y validacion

--- app2.js
+++ app2.js
@@ -1,10 +1,36 @@
-function genPDF(){
-    let doc = new jsPDF();
-    let entradaNombre = document.getElementById('entrada-nombre').value; 
-    let entradaMail = document.getElementById('entrada-mail').value; 
-    let entradaConsulta = document.getElementById('entrada-consulta').value; 
-    doc.text(20,20,entradaNombre);
-    doc.text(20,30,entradaMail);
-    doc.text(20,40,entradaConsulta);
-    doc.save('Test.pdf');
-}
+$(document).ready(function(){
+
+    var nombre, mail, consulta;
+    
+        $('#Generar-pdf').click(function(){
+
+            nombre = $('#entrada-nombre').val().trim();
+            if(nombre === "" ){
+                alert("Por favor, ingrese su nombre");
+                return;
+            }
+            mail = $('#entrada-mail').val().trim();
+            if(mail === ""){
+                alert("Por favor ingrese su mail")
+                return;
+            }
+
+            consulta = $('#entrada-consulta').val();
+            if(consulta === "")
+                alert("Por favor ingrese un mensaje");
+             return;
+        });
+        
+        
+        //Generamos el documento PDF con los datos que obtuvimos del formulario
+        $('#Generar-pdf').click(function(){
+        let doc = new jsPDF();
+
+            doc.text(20,20,nombre);
+            doc.text(20,30,mail);
+            doc.text(20,40,consulta);
+            doc.save('Test.pdf');
+        })
+    
+        
+});
